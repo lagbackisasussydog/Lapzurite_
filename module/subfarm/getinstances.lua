5b79f22e5a328a3a1b8b99879e0d441c0aaf231c
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-02-22 09:58:48 +0700

    Create getinstances.lua

---
+++ module/subfarm/getinstances.lua
@@ -0,0 +1,33 @@
+local autochest = {}
+
+function autochest:Init()
+	return function()
+		local Plr = game.Players.LocalPlayer
+		local Char = Plr.Character
+		
+		local function Tween(Inst, Info,Properties)
+			if not Inst or not Inst.Parent then return end
+			local TweenSvc = game:GetService("TweenService")
+			local Track = TweenSvc:Create(Inst, Info, Properties)
+			getgenv().Configuration.CurrentTweeningProcess = Track
+			Track:Play()
+			Track.Completed:Wait()
+		end
+		
+		while getgenv().Configuration.Modules.AutoFarmChests do
+			for _, chest in pairs(getinstances()) do
+				if chest:IsA("BasePart") and string.find(chest.Name, "Chest") then
+					if not getgenv().Configuration.Modules.AutoFarmChests then break end
+					
+					local time = Plr:DistanceFromCharacter(chest.Position) / getgenv().Configuration.TweenSpeed
+							
+					Tween(root, TweenInfo.new(time, Enum.EasingStyle.Linear), {CFrame = chest})
+					Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+					task.wait(getgenv().ModuleSetting.AutoFarmChests.Delay)
+				end
+			end
+		end
+	end
+end
+
+return autochest
