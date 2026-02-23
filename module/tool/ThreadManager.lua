805be3aa9b8ad242b2be578d4a65e46ab48c6cb9
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-02-23 18:40:52 +0700

    Update ThreadManager.lua

--- module/tool/ThreadManager.lua
+++ module/tool/ThreadManager.lua
@@ -11,8 +11,58 @@ end
 function Thread:StartThread(thread)
 	local a = Thread._thread[thread]
 	if a.Running then
+		task.cancel(a.Running)local Thread = {}
+Thread._thread = {}
+
+function Thread:AddThread(thread, func)
+	Thread._thread[thread] = {
+		["Function"] = func,
+		["Running"] = nil
+	}
+end
+
+function Thread:StartThread(thread)
+	local a = Thread._thread[thread]
+	if a.Running then
 		task.cancel(a.Running)
 		a.Running = nil
+	end
+	
+	a.Running = task.spawn(a.Function)
+end
+
+function Thread:CloseThread(thread)
+	local a = Thread._thread[thread]
+	if a.Running then
+		task.cancel(a.Running)
+		a.Running = nil
+	end
+end
+
+function Thread:ResetThread()
+	for _, v in pairs(Thread._thread) do
+		if v.Running then
+			task.cancel(v.Running)
+			v.Running = task.spawn(v.Function)
+		end
+	end
+end
+
+function Thread:ExecuteOnRuntime(thread)
+	local a = Thread._thread[thread]
+	
+	local success , err = pcall(function()
+		a.Running = task.spawn(a.Function)
+	end)
+	
+	if err then
+		print(err)
+		task.cancel(a.Running)
+		a.Running = task.spawn(a.Function)
+	end
+end
+
+return Thread
 	end
 	
 	a.Running = task.spawn(a.Function)
