2253436080431cca25e74de5bcc3bcc91907c1d3
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-02-24 14:24:17 +0700

    Update autoBone.lua

--- module/subfarm/autoBone.lua
+++ module/subfarm/autoBone.lua
@@ -2,9 +2,13 @@ local autobone ={}
 
 function autobone:Init()
 	return function()
+		local CommF_ = loadstring(game:HttpGet("https://raw.githubusercontent.com/lagbackisasussydog/Lapzurite_/refs/heads/main/module/tool/CommFManager.lua"))()
+		
+		local Plr = game.Players.LocalPlayer
 		local Enemies = workspace.Enemies
-		local Char = game.Players.LocalPlayer.Character
+		local Char = Plr.Character
 		local Root = Char.PrimaryPart or Char:FindFirstChild("HumanoidRootPart")
+		local a = CFrame.new(-9502.98145, 172.149506, 6154.40332, -0.999924958, 5.73953107e-09, -0.0122516705, 5.38207168e-09, 1, 2.92093407e-08, 0.0122516705, 2.91412086e-08, -0.999924958)
 		local MobList = {
 			"Reborn Skeleton",
 			"Living Zombie",
@@ -21,9 +25,11 @@ function autobone:Init()
 			Track.Completed:Wait()
 		end
 		
+		CommF_:requestEntrance(vector.create(-5060.41162109375, 318.50201416015625, -3193.224853515625))
+		Tween(Root, TweenInfo.new(Plr:DistanceFromCharacter(a.Position) / getgenv().Configuration.TweenSpeed, Enum.EasingStyle.Linear), {CFrame = a})
 		while getgenv().Configuration.Modules.AutoBone do
 			for _, enemy in pairs(Enemies:GetChildren()) do
-				if enemy and enemy.Humanoid.Health > 0 and MobList[enemy.Name] then
+				if enemy and enemy.Humanoid.Health > 0 and table.find(MobList, enemy.Name) then
 					local enemyRoot = enemy.PrimaryPart
 					local enemyHum = enemy.Humanoid
 					
@@ -32,7 +38,7 @@ function autobone:Init()
 						
 						repeat
 							for _, _enemy in pairs(Enemies:GetChildren()) do
-								if _enemy.Humanoid and _enemy.Humanoid.Health > 0 and _enemy.Name == enemy then
+								if _enemy.Humanoid and _enemy.Humanoid.Health > 0 and _enemy.Name == enemy.Name then
 									_enemy:PivotTo(enemy:GetPivot())
 								end
 							end
