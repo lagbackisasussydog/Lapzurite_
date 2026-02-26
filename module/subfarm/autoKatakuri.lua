b28ef25d565fc91e9bb1b00423f52017dcc09c9a
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-02-26 14:41:21 +0700

    Update autoKatakuri.lua

--- module/subfarm/autoKatakuri.lua
+++ module/subfarm/autoKatakuri.lua
@@ -57,6 +57,7 @@ function autokatakuri:Init()
 							if not Hum or Hum.Health <= 0 then break end
 							
 							Char:PivotTo(enemy:GetPivot() * CFrame.new(0,15,0))
+							Char.Humanoid:EquipTool(b:getTool())
 							b:GroupMob(enemy)
 						end
 					end
