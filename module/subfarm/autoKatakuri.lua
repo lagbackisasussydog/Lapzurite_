ba3153743b15eb0f5972468aa40091347505ca46
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-02-27 22:13:13 +0700

    Update autoKatakuri.lua

--- module/subfarm/autoKatakuri.lua
+++ module/subfarm/autoKatakuri.lua
@@ -46,32 +46,31 @@ function autokatakuri:Init()
 		
 		local function Attack()
 			local success, err = pcall(function()
-				for i, inst in ipairs({Enemies, game.ReplicatedStorage}) do
-					for _, enemy in pairs(inst:GetChildren()) do
-						if isAlive(enemy) and (table.find(MobList, enemy.Name) or table.find(BossList, enemy.Name))then
-							local eRoot = enemy.HumanoidRootPart
-							local eHum = enemy.Humanoid
-							local tool = getTool
+				for _ = 1, #Enemies:GetChildren() do
+					local enemy = Enemies:GetChildren()[1]
+					if isAlive(enemy) and table.find(MobList, enemy.Name) then
+						local eRoot = enemy.HumanoidRootPart
+						local eHum = enemy.Humanoid
+						local tool = getTool()
 							
-							if getgenv().Configuration.Modules.autokatakuri == false or Char.Humanoid.Health <= 0 then break end
+						if getgenv().Configuration.Modules.AutoKatakuri == false or Char.Humanoid.Health <= 0 then break end
 							
-							local time = TweenInfo.new(Plr:DistanceFromCharacter(eRoot.Position) / getgenv().Configuration.TweenSpeed, Enum.EasingStyle.Linear)
+						local time = TweenInfo.new(Plr:DistanceFromCharacter(eRoot.Position) / getgenv().Configuration.TweenSpeed, Enum.EasingStyle.Linear)
+						
+						Tween(Root, time, {CFrame = eRoot.CFrame * CFrame.new(0,15,0)})
 							
-							Tween(Root, time, {CFrame = eRoot.CFrame * CFrame.new(0,15,0)})
-							
-							while enemy and isAlive(enemy) and getgenv().Configuration.Modules.autokatakuri == true do
-								task.wait(0.05)
+						while enemy and isAlive(enemy) and getgenv().Configuration.Modules.AutoKatakuri == true do
+							task.wait(0.05)
 								
-								local Char = Plr.Character
-								if not Char then break end
+							local Char = Plr.Character
+							if not Char then break end
 								
-								local Hum = Char:FindFirstChild("Humanoid")
-								if not Hum or Hum.Health <= 0 then break end
+							local Hum = Char:FindFirstChild("Humanoid")
+							if not Hum or Hum.Health <= 0 then break end
 								
-								Char:PivotTo(enemy:GetPivot() * CFrame.new(0,15,0))
-								Char.Humanoid:EquipTool(tool)
-								b:GroupMob(enemy)
-							end
+							Char:PivotTo(enemy:GetPivot() * CFrame.new(0,15,0))
+							Char.Humanoid:EquipTool(tool)
+							b:GroupMob(enemy)
 						end
 					end
 				end
@@ -82,10 +81,12 @@ function autokatakuri:Init()
 		
 		game.ReplicatedStorage.Remotes.CommF_:InvokeServer("requestEntrance", vector.create(-5060.41162109375, 318.50201416015625, -3193.224853515625))
 		Char:PivotTo(Char:GetPivot() * CFrame.new(15,50,0))
+		task.wait(1)
 		Tween(Root, TweenInfo.new(Plr:DistanceFromCharacter(a.Position) / getgenv().Configuration.TweenSpeed, Enum.EasingStyle.Linear), {CFrame = a})
 		
 		while getgenv().Configuration.Modules.AutoKatakuri do
-			task.wait(0.05)
+			task.wait(0.5)
+			game.ReplicatedStorage.Remotes.CommF_:InvokeServer("CakePrinceSpawner")
 			Attack()
 		end
 	end	
