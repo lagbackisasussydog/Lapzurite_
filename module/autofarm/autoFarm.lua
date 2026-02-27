73fa416017bf892934204dcb2d8def66413540cb
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-02-27 21:59:39 +0700

    Update autoFarm.lua

--- module/autofarm/autoFarm.lua
+++ module/autofarm/autoFarm.lua
@@ -97,7 +97,7 @@ function level:Init()
 				},
 				["NPC"] = "Marine"
 			}
-		elseif lv >= 150 and lv <= 250 then
+		elseif lv >= 150 and lv <= 190 then
 			return {
 				["Target"] = {
 					{"Sky Bandit", 150, 175},
@@ -108,6 +108,124 @@ function level:Init()
 					{"SkyQuest", 2, 175, 250},
 				},
 				["NPC"] = "Marine"
+			}
+		elseif lv >= 190 and lv <= 250 then
+			return {
+				["Target"] = {
+					{"Prisoner", 190, 210},
+					{"Dangerous Prisoner", 210, 250},
+				},
+				["Quest"] = {
+					{"PrisonerQuest", 1, 190, 210},
+					{"PrisonerQuest", 2, 210, 250},
+				},
+				["NPC"] = "Jail Keeper"
+			}
+		elseif lv >= 250 and lv <= 300 then
+			return {
+				["Target"] = {
+					{"Toga Warrior", 250, 300},
+				},
+				["Quest"] = {
+					{"ColosseumQuest", 1, 250, 300},
+				},
+				["NPC"] = "Colosseum Quest Giver"
+			}
+		elseif lv >= 300 and lv <= 375 then
+			return {
+				["Target"] = {
+					{"Military Soldier", 300, 325},
+					{"Military Spy", 325, 375},
+				},
+				["Quest"] = {
+					{"MagmaQuest", 1, 300, 325},
+					{"MagmaQuest", 2, 325, 375}
+				},
+				["NPC"] = "Colosseum Quest Giver"
+			}
+		elseif lv >= 375 and lv <= 450 then
+			return {
+				["Target"] = {
+					{"Fishman Warrior", 375, 400},
+					{"Fishman Commando", 400, 450},
+				},
+				["Quest"] = {
+					{"FishmanQuest", 1, 375, 400},
+					{"FishmanQuest", 2, 400, 450}
+				},
+				["NPC"] = "King Neptune"
+			}
+		elseif lv >= 450 and lv <= 525 then
+			return {
+				["Target"] = {
+					{"God's Guard", 450, 475},
+					{"Fishman Commando", 475, 500},
+				},
+				["Quest"] = {
+					{"SkyExp1Quest", 1, 450, 475},
+					{"SkyExp1Quest", 2, 475, 500}
+				},
+				["NPC"] = "Mole"
+			}
+		elseif lv >= 525 and lv <= 625 then
+			return {
+				["Target"] = {
+					{"Royal Squad", 525, 550},
+					{"Royal Soldier", 550, 625},
+				},
+				["Quest"] = {
+					{"SkyExp2Quest", 1, 525, 550},
+					{"SkyExp2Quest", 2, 550, 625}
+				},
+				["NPC"] = "Sky Quest Giver 2"
+			}
+		elseif lv >= 625 and lv <= 700 then
+			return {
+				["Target"] = {
+					{"Galley Pirate", 625, 650},
+					{"Galley Captain", 650, 700},
+				},
+				["Quest"] = {
+					{"FountainQuest", 1, 625, 650},
+					{"FountainQuest", 2, 650, 700}
+				},
+				["NPC"] = "Freezeburg Quest Giver"
+			}
+		elseif lv >= 700 and lv <= 775 then
+			return {
+				["Target"] = {
+					{"Raider", 700, 725},
+					{"Mercenary", 725, 775},
+				},
+				["Quest"] = {
+					{"Area1Quest", 1, 700, 725},
+					{"Area1Quest", 2, 725, 775}
+				},
+				["NPC"] = "Area 1 Quest Giver"
+			}
+		elseif lv >= 775 and lv <= 875 then
+			return {
+				["Target"] = {
+					{"Swan Pirate", 775, 800},
+					{"Factory Staff", 800, 875},
+				},
+				["Quest"] = {
+					{"Area2Quest", 1, 775, 800},
+					{"Area2Quest", 2, 800, 875}
+				},
+				["NPC"] = "Area 2 Quest Giver"
+			}
+		elseif lv >= 875 and lv <= 950 then
+			return {
+				["Target"] = {
+					{"Marine Lieutenant", 875, 900},
+					{"Marine Captain", 900, 950},
+				},
+				["Quest"] = {
+					{"MarineQuest3", 1, 875, 900},
+					{"MarineQuest3", 2, 900, 950}
+				},
+				["NPC"] = "Marine Quest Giver"
 			}
 		end
 	end
@@ -121,6 +239,10 @@ function level:Init()
 				end
 			end
 		end
+	end
+	
+	local function getSpawnpoint(mob)
+	
 	end
 	
 	local function getQuest()
