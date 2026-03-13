a365250a8436cc44c4f3c3eee61d74715f0d3115
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2026-03-13 19:40:20 +0700

    Update Input.lua

--- module/tool/Input.lua
+++ module/tool/Input.lua
@@ -1,9 +1,9 @@
 local vim = {}
 
 function vim:Press(KeyCode : Enum.KeyCode, delay : number)
-	game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.Y, false, game)
+	game:GetService("VirtualInputManager"):SendKeyEvent(true, KeyCode, false, game)
 	task.wait(delay)
-	game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.Y, false, game)
+	game:GetService("VirtualInputManager"):SendKeyEvent(false, KeyCode, false, game)
 end
 
 function vim:getService()
